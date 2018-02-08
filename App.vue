e9f3a3909009bcb97f93d61a4c50f390c20e4caa
diaoshuang <diaoshuang@meituan.com>
2018-02-08 14:47:50 +0800

    add

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <draw/>
+  </div>
+</template>
+
+<script>
+import draw from './components/Draw'
+
+export default {
+  name: 'App',
+  components: {
+    draw
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #ddd;
+}
+</style>
